3989e6fcbbdd300ebf4a95ec56ce491ec224b4fd
marty-p <marty-p@outlook.com>
2018-11-28 11:27:22 +0100

    +second activity

--- EsercitazioneBonus/app/src/main/java/marty/esercitazionebonus/MainActivity.java
+++ EsercitazioneBonus/app/src/main/java/marty/esercitazionebonus/MainActivity.java
@@ -1,7 +1,11 @@
 package marty.esercitazionebonus;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final EditText inputUsername = this.findViewById(R.id.inputUsername);
+        final EditText inputPassword = this.findViewById(R.id.inputPassword);
+        Button btnAccedi = this.findViewById(R.id.btnAccedi);
+        btnAccedi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (inputUsername.toString().isEmpty() || inputPassword.toString().isEmpty()) {
+                    return;
+                }
+                Intent showResults = new Intent(MainActivity.this, FormResultActivity.class);
+                startActivity(showResults);
+            }
+        });
     }
 }
