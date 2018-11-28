96bcde3f619903afbc4775e45509ed7e90edea00
marty-p <marty-p@outlook.com>
2018-11-28 11:42:02 +0100

    +checkinput

--- EsercitazioneBonus/app/src/main/java/marty/esercitazionebonus/MainActivity.java
+++ EsercitazioneBonus/app/src/main/java/marty/esercitazionebonus/MainActivity.java
@@ -9,23 +9,48 @@ import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText inputUsername;
+    EditText inputPassword;
+    Button btnAccedi;
+    // TextView textErrors;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final EditText inputUsername = this.findViewById(R.id.inputUsername);
-        final EditText inputPassword = this.findViewById(R.id.inputPassword);
-        Button btnAccedi = this.findViewById(R.id.btnAccedi);
+        inputUsername = this.findViewById(R.id.inputUsername);
+        inputPassword = this.findViewById(R.id.inputPassword);
+
+        //textErrors = this.findViewById(R.id.textErrors);
+        //textErrors.setVisibility(View.GONE); // must be displayed only when there are errors
+
+        btnAccedi = this.findViewById(R.id.btnAccedi);
         btnAccedi.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (inputUsername.toString().isEmpty() || inputPassword.toString().isEmpty()) {
-                    return;
+                if (checkInput()) {
+                    Intent showResults = new Intent(MainActivity.this, FormResultActivity.class);
+                    startActivity(showResults);
                 }
-                Intent showResults = new Intent(MainActivity.this, FormResultActivity.class);
-                startActivity(showResults);
             }
         });
+    }
+
+    //! @brief it returns true if there are no errors
+    private boolean checkInput() {
+        boolean hasErrors = false;
+        if (inputUsername.getText().length()==0) {
+            inputUsername.setError("Inserisci l'username");
+            hasErrors = true;
+        }
+        if (inputPassword.getText().length()==0) {
+            inputPassword.setError("Inserisci la password");
+            hasErrors = true;
+        }
+        //if (hasErrors)
+        //    textErrors.setVisibility(View.VISIBLE);
+
+        return !hasErrors; // true if no errors
     }
 }
